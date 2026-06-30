26856dd1a51d8563735ceedf15e6531d79b7fccd
seokhyeon09 <ring4392@gmail.com>
2026-06-30 14:09:09 +0900

    fix: sidebarData의 사용하지 않는 아이콘 import 제거 (빌드 에러 해결)

--- src/data/sidebarData.ts
+++ src/data/sidebarData.ts
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IconSpeaker, IconDocument, IconChat, IconUser, IconBox, IconMap, IconClock, IconSearch, IconTruck, IconHome, IconConsultChat, IconHelpCircle, IconFilePlus, IconQuestion, IconInfoCircle, IconPen } from '../components/icons';
+import { IconSpeaker, IconDocument, IconChat, IconUser, IconBox, IconMap, IconClock, IconSearch, IconTruck, IconHome, IconConsultChat, IconHelpCircle, IconFilePlus } from '../components/icons';
 
 // 사이드바 전용 데이터 매핑
 // 각 top-level route (예: about, tracking, consulting, support) 별로 사이드바 구성을 정의합니다.
